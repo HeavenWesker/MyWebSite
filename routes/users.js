8c483a089b4460809d49a57cdb2c7da31f1551c6
Heaven Wesker <Heaven.Wesker@gmail.com>
2015-05-21 22:21:44 +0800

    add put

--- routes/users.js
+++ routes/users.js
@@ -15,12 +15,45 @@ router.get('/userlist', function(req, res, next) {
     //});
   });
 });
-router.post('/adduser', function(req, res, next){
+router.post('/adduser', function(req, res){
   var db = req.db;
   var collection = db.get('userlist');
   collection.insert(req.body, function(err, result){
     res.send(
-      err === null ? { msg: '' } : { msg: result }
+      err === null ? { msg: '' }: { msg: result }
+    );
+  });
+});
+router.delete('/deleteuser/:id', function(req, res, next){
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.remove({
+    //'_id': 'ObjectId(' + req.params.id + ')'
+    '_id': req.params.id 
+  }, 
+  function(err){
+    res.send(
+      err === null ? { 'msg': '' }: { 'msg': err }
+    );
+  });
+});
+router.put('/updateuser/:id', function(req, res, next){
+  console.log(req.body);
+  var db = req.db;
+  var collection = db.get('userlist');
+  var originData = collection.findById(req.params.id);
+  console.log(collection.find({'_id': req.params.id}));
+  
+  collection.updateById(req.params.id, {
+    'username': (req.body.username === null || req.body.username === '') ? originData.username : req.body.username,
+    'email': req.body.email === null ? originData.email : req.body.email,
+    'fullname': req.body.fullname == null ? originData.fullname : req.body.fullname,
+    'age': req.body.age === null ? originData.age : req.body.age,
+    'gender': req.body.gender === null ? originData.gender : req.body.gender,
+    'location': req.body.location === null ? originData.location : req.body.location
+  }, function(err){
+    res.send(
+      err === null ? { 'msg': '' } : { 'msg': err }
     );
   });
 });
